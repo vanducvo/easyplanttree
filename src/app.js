f2c8aba20e392fdaf03254155f811e096e9dd6b6
Van Duc Vo <vanducvo@gmail.com>
2020-04-11 08:08:53 +0700

    add to template engine

--- src/app.js
+++ src/app.js
@@ -2,11 +2,30 @@ const express = require('express');
 const app = express();
 const settings = require('./config/settings');
 const utils = require('./utils/utils');
+const path = require('path');
+const serveStatic = require('serve-static');
+const serveFavicon = require('serve-favicon');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res) {
+app.use(serveFavicon(path.resolve(__dirname, 'public/favicon.ico')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.use('/public', serveStatic(
+  path.resolve(__dirname, 'public'),
+  {
+    cacheControl: settings.cacheControl
+  }
+));
+
+app.get('/', function(req, res){
+  res.render('pages/index.ejs');
+});
+
+app.get('api/', function(req, res) {
   const api = {
     'home': '/',
   };
--- src/config/settings.js
+++ src/config/settings.js
@@ -2,6 +2,7 @@ const settings = {
   port: process.env.PORT || 5000,
   type: process.env.NODE_ENV || 'dev',
   logFolder: './logs',
+  cacheControl: process.env.NODE_ENV === 'development' ? false : true
 };
 
 module.exports = settings;
